40ff66ee4f2d36752ee09f64f675de10227328be
mcheng3 <michael.cheng65@gmail.com>
2018-02-25 23:24:44 +0000

    Finish matrix multiplication

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ screen = new_screen()
 color = [ 0, 255, 0 ]
 matrix = new_matrix()
 #ident(matrix)
-add_point(matrix, 2, 3, 4)
+#add_point(matrix, 2, 3, 4)
 print_matrix(matrix)
-
-
+matrix_mult(matrix, matrix)
+print_matrix(matrix)
 draw_lines( matrix, screen, color )
 display(screen)
--- matrix.py
+++ matrix.py
@@ -2,7 +2,7 @@ import math
 
 
 def print_matrix( matrix ):
-    for i in range(0,4):
+    for i in range(len(matrix[0])):
         row = ""
         for each in matrix:
            row += str(each[i]) + " "
@@ -13,20 +13,31 @@ def ident( matrix ):
     for i in range(0,4):
         matrix[i] = [0] * 4
         matrix[i][i] = 1
-            
-        
 
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    pass
+    m = [[] for x in range(0,4)]
+    m3 = []
+    row = 0
+    for r1 in range(0,4):
+        for c2 in range(0,len(m2[0])):
+            sum = 0
+            for i in range(0,len(m1[0])):
+                prod = m1[i][r1] * m2[c2][i]
+                sum += prod
+            #print str(sum) + " " + str(r1)
+            m[r1].append(sum)
+    #print_matrix(m)
+    m2[:] = list(m)
 
 
 
-
-def new_matrix(rows = 4, cols = 4):
+def new_matrix(rows = 4, cols = 4):   
     m = []
+    i = 0
     for c in range( cols ):
         m.append( [] )
         for r in range( rows ):
-            m[c].append( 0 )
+            m[c].append( i )
+            i += 1
     return m
